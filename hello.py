4ada183ffb09a4abbc9602bce969ddeccd7ab683
Vang Huynh <vanghm@imt-soft.com>
2021-05-11 23:29:40 +0700

    update file

--- hello.py
+++ hello.py
@@ -1,12 +1,37 @@
-if 5 > 2:
-    print("hello world")
-    # print("Xin chao cac ban")
-x = 10
-y = "Joy"
-print(type(x))
-print(y)
+# Tao ra dong lenh
+print("Xin chao")
 
-vang = 10
-mInh = "Minh"
-Huynh = "ABC"
-_ABC = 10
+# Phan biet khoi lenh boi khoang trang
+if 5 > 3:
+    print("5 lon hon 3 la dung")
+
+# Day la cach comment them dau '#' o truoc
+
+"""
+Python khong co khai niem comment nhieu dung, nhung ban co the su dung \"\"\"
+De tao comment nhieu dong
+Boi vi python se bo qua cac lenh string nhieu dong neu khong khi bao bien
+"""
+
+# Khai bao bien
+x = 10
+y = 2
+name = "Vang"
+_data = "data"
+print(_data)
+a = b = c = "Vang"
+print(a)
+print(b)
+print(c)
+
+e, d, f = "a", "b", "c"
+print(e)
+print(d)
+print(f)
+
+# Unpack a Collection
+data = [1, 2, 3]
+x, y , z = data
+print(x)
+print(y)
+print(z)
