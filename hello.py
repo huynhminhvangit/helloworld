3549668c2d00969f7df945b937d3e37c7dd4f5da
Vang Huynh <vanghm@imt-soft.com>
2021-09-02 21:50:44 +0700

    bai 1

--- hello.py
+++ hello.py
@@ -355,3 +355,175 @@ txt = "We are the so-called \"Vikings\" from the north."
 \ ooo	Octal value	
 \ xhh	Hex value
 """
+
+# Booleans represent one of two values: True or False.
+"""
+Boolean Values
+In programming you often need to know if an expression is True or False.
+You can evaluate any expression in Python, and get one of two answers, True or False.
+When you compare two values, the expression is evaluated and Python returns the Boolean answer:
+"""
+
+print(10 > 9)
+print(10 == 9)
+print(10 < 9)
+
+# Print a message based on whether the condition is True or False:
+a = 200
+b = 33
+
+if b > a:
+  print("b is greater than a")
+else:
+  print("b is not greater than a")
+
+# Evaluate Values and Variables
+# The bool() function allows you to evaluate any value, and give you True or False in return,
+# Evaluate a string and a number:
+print(bool("Hello"))
+print(bool(15))
+
+# Evaluate two variables:
+x = "Hello"
+y = 15
+
+print(bool(x))
+print(bool(y))
+
+"""
+Most Values are True
+Almost any value is evaluated to True if it has some sort of content.
+Any string is True, except empty strings.
+Any number is True, except 0.
+Any list, tuple, set, and dictionary are True, except empty ones.
+"""
+bool("abc")
+bool(123)
+bool(["apple", "cherry", "banana"])
+
+# In fact, there are not many values that evaluate to False, except empty values, such as (), [], {}, "", the number 0, and the value None. And of course the value False evaluates to False.
+# The following will return False:
+bool(False)
+bool(None)
+bool(0)
+bool("")
+bool(())
+bool([])
+bool({})
+
+# One more value, or object in this case, evaluates to False, and that is if you have an object that is made from a class with a __len__ function that returns 0 or False:
+class myclass():
+  def __len__(self):
+    return 0
+
+myobj = myclass()
+print(bool(myobj))
+
+
+# Functions can Return a Boolean
+# You can create functions that returns a Boolean Value:
+def myFunction() :
+  return True
+
+print(myFunction())
+
+# You can execute code based on the Boolean answer of a function:
+# Print "YES!" if the function returns True, otherwise print "NO!":
+def myFunction() :
+  return True
+
+if myFunction():
+  print("YES!")
+else:
+  print("NO!")
+
+
+# Python also has many built-in functions that return a boolean value, like the isinstance() function, which can be used to determine if an object is of a certain data type:
+x = 200
+print(isinstance(x, int))
+
+# Python Operators
+"""
+Python divides the operators in the following groups:
+
+- Arithmetic operators
+- Assignment operators
+- Comparison operators
+- Logical operators
+- Identity operators
+- Membership operators
+- Bitwise operators
+"""
+
+"""
+Operator	Name	              Example
++ 	      Addition	          x + y	
+-	        Subtraction	        x - y	
+*	        Multiplication	    x * y	
+/	        Division	          x / y	
+%	        Modulus	            x % y	
+**	      Exponentiation	    x ** y	
+//	      Floor division	    x // y
+"""
+
+# Python Assignment Operators
+# Assignment operators are used to assign values to variables:
+"""
+Operator	  Example	        Same As
+=	          x = 5	          x = 5	
++=	        x += 3	        x = x + 3	
+-=	        x -= 3	        x = x - 3	
+*=	        x *= 3	        x = x * 3	
+/=	        x /= 3	        x = x / 3	
+%=	        x %= 3	        x = x % 3	
+//=	        x //= 3	        x = x // 3	
+**=	        x **= 3	        x = x ** 3	
+&=	        x &= 3	        x = x & 3	
+|=	        x |= 3	        x = x | 3	
+^=	        x ^= 3	        x = x ^ 3	
+>>=	        x >>= 3	        x = x >> 3	
+<<=	        x <<= 3	        x = x << 3
+"""
+
+
+# Python Comparison Operators
+"""
+Operator	      Name	                        Example
+==	            Equal	                        x == y	
+!=	            Not equal	                    x != y	
+>	              Greater than	                x > y	
+<	              Less than	                    x < y	
+>=	            Greater than or equal to	    x >= y	
+<=	            Less than or equal to	        x <= y
+"""
+
+# Exercise:
+# Print the second item in the fruits list.
+
+fruits = ["apple", "banana", "cherry"]
+print(fruits)
+
+# Change the value from "apple" to "kiwi", in the fruits list.
+fruits = ["apple", "banana", "cherry"]
+fruits[0] = "kiwi"
+print(fruits)
+# Use the append method to add "orange" to the fruits list.
+fruits = ["apple", "banana", "cherry"]
+fruits.append("orange")
+print(fruits)
+# Use the insert method to add "lemon" as the second item in the fruits list.
+fruits.insert(1, "lemon")
+print(fruits)
+# Use the remove method to remove "banana" from the fruits list.
+fruits = ["apple", "banana", "cherry"]
+fruits.remove("banana")
+print(fruits)
+# Use negative indexing to print the last item in the list.
+fruits = ["apple", "banana", "cherry"]
+print(fruits[-1])
+# Use a range of indexes to print the third, fourth, and fifth item in the list.
+fruits = ["apple", "banana", "cherry", "orange", "kiwi", "melon", "mango"]
+print(fruits[2:5])
+# Use the correct syntax to print the number of items in the list.
+fruits = ["apple", "banana", "cherry"]
+print(len(fruits))
